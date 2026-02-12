f0759a8fe4cfe25e97981d4c79a6f6a7951aa36c
annablomberg00@gmail.com <annablomberg00@gmail.com>
2026-02-12 11:35:23 +0100

    Add frontend and update gitignore

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+      return <h1 className="text-3xl text-sm text-red-400 font-bold">Hello Tailwind v4</h1>;
+    </>
+  )
+}
+
+export default App
