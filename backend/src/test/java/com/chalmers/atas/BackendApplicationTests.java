f1526478f98058b5def7d4b404295b266866ec56
neoz-chalmers <neoz@chalmers.se>
2026-02-12 09:38:41 +0100

    init commit
    Initialized Spring Boot backend app using
    postgres + flyway (for versioning). Using
    Spring Boot 3.5.10 as 4.x is brand new and
    using an older more stable version is probably
    better for our use case. Java version is 21.
    
    Added swagger for easier FE development.
    
    Also configured `docker-compose.yaml` to build
    a compose stack consisting of postgres and BE.

---
+++ backend/src/test/java/com/chalmers/atas/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.chalmers.atas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BackendApplicationTests {
+    @Test
+    void contextLoads() { }
+}
