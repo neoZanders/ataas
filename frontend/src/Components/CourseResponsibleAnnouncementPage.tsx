16d087ffeae46cf2f92c8bf0dd8a8d2529702ba2
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2026-03-04 16:32:48 +0100

    initilize CR announcement page with routing

---
+++ frontend/src/Components/CourseResponsibleAnnouncementPage.tsx
@@ -0,0 +1,9 @@
+import SideTabNav from "./SideTabNav"
+
+
+export function CourseResponsibleAnnouncementPage(){
+    return <div className="min-h-screen bg-stone-50">
+            <SideTabNav />
+
+        </div>
+}
