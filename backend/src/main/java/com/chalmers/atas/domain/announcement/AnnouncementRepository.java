17760ad7935cf1d22123881bd5456abfa9ab0dff
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2026-03-20 10:13:33 +0100

    add Announcement Repo

---
+++ backend/src/main/java/com/chalmers/atas/domain/announcement/AnnouncementRepository.java
@@ -0,0 +1,10 @@
+package com.chalmers.atas.domain.announcement;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnnouncementRepository extends JpaRepository<Announcement, UUID>{
+    List<Announcement> findByCourseIdOrderByCreatedAtDesc(UUID courseId);
+}
