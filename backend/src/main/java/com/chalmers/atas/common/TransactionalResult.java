9f985567f81c81306b04aefbd9dca7e73a16de5b
Neo Zanders <neozanders@gmail.com>
2026-03-21 19:43:41 +0100

    Add some more utils

--- backend/src/main/java/com/chalmers/atas/common/TransactionalResult.java
+++ backend/src/main/java/com/chalmers/atas/common/TransactionalResult.java
@@ -2,6 +2,9 @@ package com.chalmers.atas.common;
 
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class TransactionalResult<T> extends Result<T> {
     private TransactionalResult(boolean success, T data, Error error) {
         super(success, data, error);
@@ -9,6 +12,10 @@ public class TransactionalResult<T> extends Result<T> {
 
     public static <T> TransactionalResult<T> ok(T data) {
         return new TransactionalResult<>(true, data, null);
+    }
+
+    public static TransactionalResult<Void> ok() {
+        return new TransactionalResult<>(true, null, null);
     }
 
     public static <T> TransactionalResult<T> rollbackFor(Error error) {
@@ -23,5 +30,38 @@ public class TransactionalResult<T> extends Result<T> {
             return TransactionalResult.ok(result.getData());
         }
         return TransactionalResult.rollbackFor(result.getError());
+    }
+
+    @Override
+    public <R> TransactionalResult<R> map(Function<T, R> mapper) {
+        if (isSuccess()) {
+            return TransactionalResult.ok(mapper.apply(getData()));
+        }
+        return TransactionalResult.rollbackFor(getError());
+    }
+
+    @Override
+    public <R> TransactionalResult<R> flatMap(Function<T, Result<R>> mapper) {
+        if (isSuccess()) {
+            return TransactionalResult.from(mapper.apply(getData()));
+        }
+        return TransactionalResult.rollbackFor(getError());
+    }
+
+    @Override
+    public TransactionalResult<Void> then(Consumer<? super T> action) {
+        if (isSuccess()) {
+            action.accept(getData());
+            return TransactionalResult.ok();
+        }
+        return TransactionalResult.rollbackFor(getError());
+    }
+
+    @Override
+    public TransactionalResult<T> peek(Consumer<? super T> action) {
+        if (isSuccess()) {
+            action.accept(getData());
+        }
+        return this;
     }
 }
