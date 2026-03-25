9bcd156f9b8c8731a99653948f334554f9c52c9c
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2026-03-25 19:48:46 +0100

    implement announcement domain service

---
+++ backend/src/main/java/com/chalmers/atas/domain/announcement/AnnouncementService.java
@@ -0,0 +1,33 @@
+package com.chalmers.atas.domain.announcement;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.chalmers.atas.common.Result;
+import com.chalmers.atas.common.TransactionalResult;
+import com.chalmers.atas.domain.course.Course;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AnnouncementService {
+
+    private final AnnouncementRepository announcementRepository;
+
+    public Result<List<Announcement>> getAnnouncements(Course course) {
+        return Result.ok(announcementRepository.findByCourseIdOrderByCreatedAtDesc(course.getCourseId()));
+    }
+
+    @Transactional
+    public TransactionalResult<Announcement> createAnnouncement(
+            Course course,
+            String title,
+            String body,
+            boolean sendByEmail
+    ) {
+        return TransactionalResult.ok(announcementRepository.save(Announcement.of(course, title, body, sendByEmail)));
+    }
+}
