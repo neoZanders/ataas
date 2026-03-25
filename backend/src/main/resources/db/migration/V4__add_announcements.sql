392bac9550c278f33f5b810eaef14505a800e07b
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2026-03-25 20:26:20 +0100

    add db migration for announcemetns

---
+++ backend/src/main/resources/db/migration/V4__add_announcements.sql
@@ -0,0 +1,13 @@
+CREATE TABLE announcements (
+    announcement_id UUID PRIMARY KEY,
+    course_id UUID NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    announcement TEXT NOT NULL,
+    send_by_email BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT fk_announcements_course
+        FOREIGN KEY (course_id)
+            REFERENCES courses(course_id)
+            ON DELETE CASCADE
+);
