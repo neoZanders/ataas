83c7ef4be557dc5a58e99ee3d1169b6f87906754
Uy Vuong <mai.uyvuong@gmail.com>
2026-03-26 18:31:36 +0100

    implemented all the data needed for sessions for the algorithm

--- backend/src/main/java/com/chalmers/atas/algorithm/model/Sessions.java
+++ backend/src/main/java/com/chalmers/atas/algorithm/model/Sessions.java
@@ -1,4 +1,32 @@
 package com.chalmers.atas.algorithm.model;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Sessions {
+    private final String courseId;
+    private final String sessionId;
+    private final Set<String> timeSlots;
+    private final int durationTime;
+    private final int requiredAmountTa;
+
+
+    public Sessions(String courseId, String sessionId, Set<String> timeSlots, int durationTime, int requiredAmountTa){
+        this.courseId = courseId;
+        this.sessionId = sessionId;
+        this.timeSlots = Collections.unmodifiableSet(new HashSet<>(timeSlots));
+        this.durationTime = durationTime;
+        this.requiredAmountTa = requiredAmountTa;
+    }
+
+    public String getCourseId(){return courseId;}
+    public String getSessionId(){return sessionId;}
+
+    public Set<String> getTimeSlots(){return timeSlots;}
+
+    public int getDurationTime(){return durationTime;}
+
+    public int getRequiredAmountTa(){return requiredAmountTa;}
+
 }
