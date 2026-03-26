40382435bc43e6173da87ddc943bf589e8c9e5c9
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2026-03-26 16:46:21 +0100

    add owner

--- backend/src/main/resources/db/migration/V4__add_announcements.sql
+++ backend/src/main/resources/db/migration/V4__add_announcements.sql
@@ -1,6 +1,7 @@
 CREATE TABLE announcements (
     announcement_id UUID PRIMARY KEY,
     course_id UUID NOT NULL,
+    owner_id UUID NOT NULL,
     title VARCHAR(255) NOT NULL,
     announcement TEXT NOT NULL,
     send_by_email BOOLEAN NOT NULL DEFAULT FALSE,
@@ -9,5 +10,9 @@ CREATE TABLE announcements (
     CONSTRAINT fk_announcements_course
         FOREIGN KEY (course_id)
             REFERENCES courses(course_id)
-            ON DELETE CASCADE
+            ON DELETE CASCADE,
+    
+    CONSTRAINT fk_announcements_owner
+        FOREIGN KEY (owner_id)
+            REFERENCES users(user_id)
 );
