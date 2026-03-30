55a4df76bcaaaef037f10bcd381a98dd2424680f
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2026-03-30 19:03:29 +0200

    add TA course assignment repo

---
+++ backend/src/main/java/com/chalmers/atas/domain/tacourseassignment/TACourseAssignmentRepository.java
@@ -0,0 +1,9 @@
+package com.chalmers.atas.domain.tacourseassignment;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TACourseAssignmentRepository extends JpaRepository<TACourseAssignment, UUID>{
+
+}
