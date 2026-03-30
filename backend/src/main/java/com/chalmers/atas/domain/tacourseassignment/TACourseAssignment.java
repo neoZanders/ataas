c19b1ae326160d85b23a4d14144e228a79af3403
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2026-03-30 18:53:51 +0200

    add TACourseAssignment entity

---
+++ backend/src/main/java/com/chalmers/atas/domain/tacourseassignment/TACourseAssignment.java
@@ -0,0 +1,88 @@
+package com.chalmers.atas.domain.tacourseassignment;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import com.chalmers.atas.domain.course.Course;
+import com.chalmers.atas.domain.coursesession.CourseSession.CourseSessionType;
+import com.chalmers.atas.domain.user.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ta_course_assignments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TACourseAssignment implements Serializable {
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "UUID")
+    private UUID taCourseAssignmentId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ta_user_id", nullable = false)
+    private User ta;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TAAssignmentStatus status;
+
+    @Column(nullable = false)
+    private int minHours;
+    
+    @Column(nullable = false)
+    private int maxHours;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CourseSessionType sessionTypePreference;
+
+    @Column
+    private Boolean isCompactSchedule;
+
+    public enum TAAssignmentStatus{
+        INVITED,
+        JOINED
+    }
+
+    public static TACourseAssignment of(
+            User ta,
+            Course course,
+            TAAssignmentStatus status,
+            int minHours,
+            int maxHours,
+            CourseSessionType sessionTypePreference,
+            Boolean isCompactSchedule
+        ){
+        TACourseAssignment taCourseAssignment = new TACourseAssignment();
+        taCourseAssignment.ta = ta;
+        taCourseAssignment.course = course;
+        taCourseAssignment.status = status;
+        taCourseAssignment.minHours = minHours;
+        taCourseAssignment.maxHours = maxHours;
+        taCourseAssignment.sessionTypePreference = sessionTypePreference;
+        taCourseAssignment.isCompactSchedule = isCompactSchedule;
+        return taCourseAssignment;
+    }
+
+
+}
