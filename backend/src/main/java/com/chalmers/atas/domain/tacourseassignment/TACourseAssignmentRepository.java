24faa4b32e57f3115f6a5cb981bec0e46ed07cb9
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2026-04-01 16:26:28 +0200

    add necessary queries in repo

--- backend/src/main/java/com/chalmers/atas/domain/tacourseassignment/TACourseAssignmentRepository.java
+++ backend/src/main/java/com/chalmers/atas/domain/tacourseassignment/TACourseAssignmentRepository.java
@@ -1,9 +1,21 @@
 package com.chalmers.atas.domain.tacourseassignment;
 
+import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TACourseAssignmentRepository extends JpaRepository<TACourseAssignment, UUID>{
+import com.chalmers.atas.domain.course.Course;
+import com.chalmers.atas.domain.user.User;
 
+
+public interface TACourseAssignmentRepository extends JpaRepository<TACourseAssignment, UUID>{
+    Optional<TACourseAssignment> findByTaAndCourse(User ta, Course course);
+
+    boolean existsByTaAndCourse(User ta, Course course);
+
+    List<TACourseAssignment> findAllByCourse(Course course);
+
+    List<TACourseAssignment> findAllByTa(User ta);
 }
