7a6f4ca280e0b2deec67beac785e4f683a1be3ec
Neo Zanders <neozanders@gmail.com>
2026-04-26 20:30:48 +0200

    Fix API mismatch

--- backend/src/main/java/com/chalmers/atas/api/taconstraint/TAConstraintsResponse.java
+++ backend/src/main/java/com/chalmers/atas/api/taconstraint/TAConstraintsResponse.java
@@ -1,14 +1,12 @@
 package com.chalmers.atas.api.taconstraint;
 
 import com.chalmers.atas.api.user.UserResponse;
-import com.chalmers.atas.domain.course.Course;
 import com.chalmers.atas.domain.tacoursesessionconstraint.TACourseSessionConstraint;
 import com.chalmers.atas.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -16,17 +14,13 @@ public class TAConstraintsResponse {
 
     private UserResponse ta;
 
-    private UUID courseId;
-
     List<TAConstraintResponse> taConstraints;
 
     public static TAConstraintsResponse of(
             User ta,
-            Course course,
             List<TACourseSessionConstraint> taConstraints) {
         return new TAConstraintsResponse(
                 UserResponse.of(ta),
-                course.getCourseId(),
                 taConstraints.stream().map(TAConstraintResponse::of).toList()
         );
     }
