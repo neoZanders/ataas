6fd4b6b3559ded25a15a65a77d6d000bd6ad196c
Nova Alldén <nova.allden@gmail.com>
2026-04-28 10:16:53 +0200

    Add explicit CR access rule for schedule POST & small changes in CR Main Page frontend

--- frontend/src/Components/CR/CourseResponsibleMainPage.tsx
+++ frontend/src/Components/CR/CourseResponsibleMainPage.tsx
@@ -42,7 +42,7 @@ export function CourseResponsibleMainPage() {
         <div className="min-h-screen bg-stone-50">
             <SideTabNav />
             <main className="pl-[104px] pt-6">
-                <div className="flex items-center justify-center mb-6">
+                <div className="mb-6 flex flex-col items-center justify-center gap-3">
                 <button
                     className="rounded-2xl bg-[#003b5c] text-xl font-medium text-slate-50 hover:bg-[#002741] px-10 py-2 cursor-pointer"
                     type="button">
